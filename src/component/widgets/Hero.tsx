cde464ceeba8bb9e82cc429b842286cd27954a92
Bilal0335 <bilalhussain42201.com>
2024-10-13 00:00:10 +0500

    update 😄

--- src/component/widgets/Hero.tsx
+++ src/component/widgets/Hero.tsx
@@ -6,20 +6,20 @@ const Hero = () => {
     <section className="">
       <Wrapper>
         {/* left side */}
-        <div className="flex items-center justify-center mt-5">
+        <div className="flex flex-col items-center justify-center mt-5 md:flex-row">
           <div className="flex-1">
             <h4 className="text-lg font-semibold text-teal-800">
               Presidential Initiative for Artificial Intelligence and Computing
               (PIAIC)
             </h4>
-            <h1 className="text-5xl font-bold leading-tight">
+            <h1 className="text-5xl font-bold leading-tight text-gray-900 sm:text-6xl">
               Certified Web 3.0 and Metaverse Developer
             </h1>
             <p className="mt-4 text-lg text-gray-500">
               A One and Quarter Years Panaverse DAO Earn as you Learn Program
               Getting Ready for the Next Generation of the Internet
             </p>
-            
+
             <p className="mt-4 text-lg text-gray-500">
               Consolidating Web 3.0, Metaverse, Artificial Intelligence (AI),
               Cloud, Edge, Ambient Computing/IoT, Network Automation, and
