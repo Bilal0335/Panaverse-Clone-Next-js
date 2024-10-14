3423c473891771d3c9aca979470e1107b734c23b
Bilal0335 <bilalhussain42201.com>
2024-10-14 21:25:38 +0500

    😄 update 😄

---
+++ src/component/shared/QuaterBox.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+const QuaterBox: FC<{ title: string; desc: string; number: number }> = ({
+  title,
+  desc,
+  number 
+}) => {
+  return (
+    <div className="">
+      <div className="relative px-10 py-10 border rounded-md">
+        <h4 className="text-lg font-bold">{title}</h4>
+        <p className="mt-2 text-slate-600">{desc}</p>
+        <span className="absolute top-0 font-bold text-gray-200 text-9xl right-10 -z-10">
+          {number}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default QuaterBox;
