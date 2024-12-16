4a91eb491147f41d0b47c8d4d65bee5e7119f4be
Bilal0335 <bilalhussain42201.com>
2024-12-16 17:27:06 +0500

    😄 update 😄

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,21 @@
-import Footer from "@/component/layout/Footer"; // Corrected path
+import Footer from "@/component/layout/Footer";
 import "./globals.css";
-import Header from "@/component/layout/Header"; // Corrected path
+import Header from "@/component/layout/Header";
+import { Metadata } from "next"; 
+
+export const metadata: Metadata = {
+  title: "Panaverse DAO",
+  description: "Certified Web 3.0 and Metaverse Developer",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
